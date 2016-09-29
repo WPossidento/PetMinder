a00202a843b5e4a898939c5baac841dad9b5e750
Olivia <oliviantaylor@gmail.com>
2016-09-29 14:26:50 -0400

    tab bar change

--- PetManager/TabBarController.m
+++ PetManager/TabBarController.m
@@ -19,22 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-  
+    //Tab Bar
     [[UITabBar appearance] setBarTintColor:[UIColor blackColor]];
-//    [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-//                                                       [UIColor whiteColor], NSForegroundColorAttributeName,
-//                                                       nil] forState:UIControlStateNormal];
-    
     UIViewController *taskListViewController = [[TaskListViewController alloc] initWithNibName:@"TaskListViewController" bundle:nil];
     taskListViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Tasks" image:[UIImage imageNamed:@"text-list"] tag:0];
-//    taskListViewController.tabBarItem.image = [[UIImage imageNamed:@"text-list"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
+
     UIViewController *petListViewController = [[PetListViewController alloc] initWithNibName:@"PetListViewController" bundle:nil];
     petListViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Pets" image:[UIImage imageNamed:@"pet-dog"] tag:0];
-//    petListViewController.tabBarItem.image = [[UIImage imageNamed:@"pet-dog"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 
     [[UITabBar appearance] setTintColor:[UIColor colorWithRed:0.00 green:0.65 blue:1.00 alpha:1.0]];
     
+    //Navigation controllers
     UINavigationController *navctl1 = [[UINavigationController alloc]init];
     [navctl1 setViewControllers:@[taskListViewController]];
     UINavigationController *navctl2 = [[UINavigationController alloc]init];
@@ -45,7 +40,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
